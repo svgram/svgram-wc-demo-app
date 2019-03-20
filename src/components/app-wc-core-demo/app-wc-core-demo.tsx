4da736a372a3e006e2e46cd529717202a4f14591
rasor <soren@rasor.dk>
2019-03-20 20:29:10 +0100

    installed "@svgram/svgram-wc-core": "0.0.2"

--- src/components/app-wc-core-demo/app-wc-core-demo.tsx
+++ src/components/app-wc-core-demo/app-wc-core-demo.tsx
@@ -1,3 +1,7 @@
+/// <reference types="@svgram/svgram-wc-core" />
+// Above ref is needed when @svgram/svgram-wc-core is used in markup instead of as code like
+// import  SvgramCanvas  from '@svgram/svgram-wc-core';
+// It will use the types from /node_modules/@svgram/svgram-wc-core/dist/types/components.d.ts and import them into /src/components.d.ts
 import { Component, Prop } from '@stencil/core';
 import { MatchResults } from '@stencil/router';
 
